ea60c1c40da379c42ab439d0859c55bfbf51164e
CalistaFox <miomoor182@gmail.com>
2026-01-06 00:42:35 +0300

    Kalitech 0.1.8-SNAPSHOT  Core

--- kalitech-core/src/main/java/org/foxesworld/kalitech/core/KalitechVersion.java
+++ kalitech-core/src/main/java/org/foxesworld/kalitech/core/KalitechVersion.java
@@ -3,7 +3,7 @@ package org.foxesworld.kalitech.core;
 public final class KalitechVersion {
     public static final String ASSETSDIR = "assets";
     public static final String NAME = "KALITECH CORE";
-    public static final String VERSION = "0.1.7-SNAPSHOT";
+    public static final String VERSION = "0.1.8-SNAPSHOT";
 
     private KalitechVersion() {}
 }
